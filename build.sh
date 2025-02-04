59944a7dd882bb68757fcd1cb7165d5d13fb81d8
Tiago Soares <tl.soares@campus.fct.unl.pt>
2025-02-04 17:04:05 +0100

    Notes theme inserted during compilation

--- build.sh
+++ build.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 cd content
 wget -O econfig.org $CONFIG
+echo "#+SETUPFILE: https://fniessen.github.io/org-html-themes/org/theme-readtheorg.setup" >> config.org
 cd ..
 emacs -Q --script build.el
 cp -n images/* public/
+sed -i '$ d' ./content/config.org
