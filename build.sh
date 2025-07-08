caeaa29be77cf281f154c03fdf74457388f1953b
Tiago Soares <tl.soares@campus.fct.unl.pt>
2025-07-09 00:11:50 +0200

    Also retrieves config for EXWM

--- build.sh
+++ build.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 cd content
 wget -O econfig.org $CONFIG
+wget -O exwm.org $EXWM_CONFIG
 echo "#+SETUPFILE: https://fniessen.github.io/org-html-themes/org/theme-readtheorg.setup" >> config.org
+echo -e "* EXWM\n#+INCLUDE: exwm.org" >> econfig.org
 cd ..
 emacs -Q --script build.el
 cp -n images/* public/
